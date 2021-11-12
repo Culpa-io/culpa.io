728a22690f837c4f277980c6556453faa4c6da15
Radilx <sam22951494@gmail.com>
2021-11-12 09:19:53 -0500

    code reorganization

---
+++ static/js/header.js
@@ -0,0 +1,7 @@
+document.querySelector('#sbi').onclick = function(){
+    window.open('https://github.com/Culpa-io/culpa.io', '_');
+}
+
+document.querySelector('#himg').onclick = function(){
+    window.location.href = '/';
+}
